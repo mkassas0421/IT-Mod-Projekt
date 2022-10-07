4fbb78e34b271cd293d6e446252f51183b93b90d
Csordás Barna <barnacsordas@gmail.com>
2022-10-07 16:39:37 +0200

    autoscale valtoztatasa

--- CsordasBarna_BeadandoDolgozat/FormAdminFelület.Designer.cs
+++ CsordasBarna_BeadandoDolgozat/FormAdminFelület.Designer.cs
@@ -374,8 +374,7 @@
             // 
             // FormAdminFelület
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.ClientSize = new System.Drawing.Size(1493, 710);
             this.Controls.Add(this.panel3);
