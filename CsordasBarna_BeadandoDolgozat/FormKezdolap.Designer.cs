4813195da7302fe0b144d7c917bdc583428834a6
mkassas0421 <mkassas0421@gmail.com>
2022-10-11 14:36:46 +0200

    Gráf

--- CsordasBarna_BeadandoDolgozat/FormKezdolap.Designer.cs
+++ CsordasBarna_BeadandoDolgozat/FormKezdolap.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace CsordasBarna_BeadandoDolgozat
+﻿
+namespace CsordasBarna_BeadandoDolgozat
 {
     partial class FormKezdolap
     {
@@ -28,21 +29,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormKezdolap));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
             this.lblOsszesVasarlo = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.label10 = new System.Windows.Forms.Label();
-            this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
             this.label8 = new System.Windows.Forms.Label();
+            this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+            this.label10 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label12 = new System.Windows.Forms.Label();
             this.panel5 = new System.Windows.Forms.Panel();
             this.label9 = new System.Windows.Forms.Label();
@@ -51,12 +57,15 @@
             this.label13 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
+            this.formKezdolapBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.panel5.SuspendLayout();
             this.panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.formKezdolapBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -81,15 +90,16 @@
             this.panel1.Size = new System.Drawing.Size(260, 116);
             this.panel1.TabIndex = 1;
             // 
-            // label2
+            // label4
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(13, 18);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(102, 23);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Bevételek";
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(176)))));
+            this.label4.Location = new System.Drawing.Point(14, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Legutóbbi 30 nap";
             // 
             // label3
             // 
@@ -102,16 +112,15 @@
             this.label3.TabIndex = 1;
             this.label3.Text = "1 200 000 Ft";
             // 
-            // label4
+            // label2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(176)))));
-            this.label4.Location = new System.Drawing.Point(14, 86);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(122, 17);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Legutóbbi 30 nap";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(13, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Bevételek";
             // 
             // panel2
             // 
@@ -167,15 +176,16 @@
             this.panel3.Size = new System.Drawing.Size(260, 282);
             this.panel3.TabIndex = 1;
             // 
-            // label10
+            // label8
             // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label10.Location = new System.Drawing.Point(13, 18);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(186, 23);
-            this.label10.TabIndex = 0;
-            this.label10.Text = "Bevétel növekedés";
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(176)))));
+            this.label8.Location = new System.Drawing.Point(59, 219);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(126, 17);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Legutóbbi 30 nap ";
             // 
             // circularProgressBar1
             // 
@@ -209,25 +219,46 @@
             this.circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(5, 8, 0, 0);
             this.circularProgressBar1.Value = 67;
             // 
-            // label8
+            // label10
             // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(176)))));
-            this.label8.Location = new System.Drawing.Point(59, 219);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(126, 17);
-            this.label8.TabIndex = 2;
-            this.label8.Text = "Legutóbbi 30 nap ";
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.Location = new System.Drawing.Point(13, 18);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(186, 23);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Bevétel növekedés";
             // 
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(67)))));
+            this.panel4.Controls.Add(this.chart1);
             this.panel4.Controls.Add(this.label12);
             this.panel4.Location = new System.Drawing.Point(299, 173);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(563, 217);
             this.panel4.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(67)))));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(79, 18);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Light;
+            series1.BorderColor = System.Drawing.Color.Black;
+            series1.ChartArea = "ChartArea1";
+            series1.LabelForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(67)))));
+            series1.Legend = "Legend1";
+            series1.MarkerBorderColor = System.Drawing.Color.White;
+            series1.Name = "Bevetel";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(409, 184);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
             // 
             // label12
             // 
@@ -312,6 +343,10 @@
             this.label15.Text = "X";
             this.label15.Click += new System.EventHandler(this.label15_Click);
             // 
+            // formKezdolapBindingSource
+            // 
+            this.formKezdolapBindingSource.DataSource = typeof(CsordasBarna_BeadandoDolgozat.FormKezdolap);
+            // 
             // FormKezdolap
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -339,10 +374,12 @@
             this.panel3.PerformLayout();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
             this.panel6.ResumeLayout(false);
             this.panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.formKezdolapBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -372,5 +409,7 @@
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.BindingSource formKezdolapBindingSource;
     }
 }
--- CsordasBarna_BeadandoDolgozat/FormKezdolap.cs
+++ CsordasBarna_BeadandoDolgozat/FormKezdolap.cs
@@ -40,6 +40,88 @@ namespace CsordasBarna_BeadandoDolgozat
                 MessageBox.Show("Nem sikerült a termékek beolvasása a Regisztraltak.txt fájlból!");
                 throw;
             }
+
+            StreamReader term = new StreamReader("Termekek.txt", Encoding.UTF8);
+            List<Sajat> beolvasottTerm = new List<Sajat>();
+            while (!term.EndOfStream)
+            {
+                string[] split = term.ReadLine().Split(';');
+                if (split.Length == 5)
+                {
+                    //sajat termek
+                    Sajat t = new Sajat(split[0], split[1], Convert.ToInt32(split[2]), Convert.ToInt32(split[3]), split[4]);
+                    beolvasottTerm.Add(t);
+
+                }
+                else
+                {
+                    //beszerzett termek
+                    Beszerzett t = new Beszerzett(split[0], split[1], Convert.ToInt32(split[2]), Convert.ToInt32(split[3]), split[4], split[5]);
+                    beolvasottTerm.Add(t);
+
+                }
+            }
+            term.Close();
+
+            //Series series = this.chartControl.Series.Add("Total Income");
+            //series.ChartType = SeriesChartType.Spline;
+
+            /*chart1.Series["Bevetel"].Points.AddXY(beolvasottTerm[0].Dátum, beolvasottTerm[0].Ár);
+            chart1.Series["Bevetel"].Points.AddXY(beolvasottTerm[1].Dátum, beolvasottTerm[1].Ár);
+            chart1.Series["Bevetel"].Points.AddXY(beolvasottTerm[2].Dátum, beolvasottTerm[2].Ár);
+            chart1.Series["Bevetel"].Points.AddXY(beolvasottTerm[3].Dátum, beolvasottTerm[3].Ár);
+            chart1.Series["Bevetel"].Points.AddXY(beolvasottTerm[4].Dátum, beolvasottTerm[4].Ár);*/
+            chart1.ChartAreas[0].BackColor = Color.FromArgb(37, 42, 67);
+            List<Tuple<DateTime, int>> list_bev = new List<Tuple<DateTime, int>>();
+            var usersGroupedByCountry = beolvasottTerm.GroupBy(user => user.Dátum);
+            foreach (var group in usersGroupedByCountry)
+            {
+                //Console.WriteLine("Dátum " + group.Key + ":");
+                DateTime myDate = DateTime.ParseExact(group.Key, "yyyy. MM. dd.",
+                                       System.Globalization.CultureInfo.InvariantCulture);
+                int groupbyAr = 0;
+                foreach (var user in group)
+                    groupbyAr += user.Ár;
+                list_bev.Add(new Tuple<DateTime, int>(myDate, groupbyAr));
+
+                //Console.WriteLine("Bevételek: " + groupbyAr);
+            }
+
+            foreach (var i in list_bev)
+            {
+                Console.WriteLine(i.Item1.ToString("yyyy. MM. dd.") + "  " + i.Item2);
+
+            }
+            var listOrdered = list_bev.OrderBy(x => x.Item1).ToList();
+
+            Console.WriteLine();
+            foreach (var i in listOrdered)
+            {
+
+                chart1.Series["Bevetel"].Points.AddXY(i.Item1.ToString("yyyy. MM. dd."), i.Item2);
+                Console.WriteLine(i.Item1.ToString("yyyy. MM. dd.") + "  " + i.Item2);
+
+            }
+            /*
+            List<String> x_tengely = beolvasottTerm.Select(i => i.Dátum).Distinct().ToList<String>();
+            x_tengely.ForEach(i => Console.WriteLine(i));
+            List<Tuple<string, int>> list_bev = new List<Tuple<string,int>>();
+            int count_x = 0;
+            foreach (String i in x_tengely){
+                foreach(Sajat j in beolvasottTerm){
+                    if (i.Equals(j.Dátum))
+                    {
+                        int napi_bev = list_bev[count_x].Item2;
+                        list_bev.Add(new Tuple<string, int>(i, list_bev.get));
+                    }
+                }
+                chart1.Series["Bevetel"].Points.AddXY(list_bev[count_x].Item1, list_bev[count_x].Item2);
+                count_x++;
+            }
+            */
+
+            chart1.Titles.Add("Napi bevétel gráf");
+
             string s = count.ToString();
             lblOsszesVasarlo.Text = s;
         }
